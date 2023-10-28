1454518420ead41d401bfac60533f67a2bb8fb8a
Parviz <Parvi3@yandex.ru>
2023-10-29 02:55:54 +0300

    исправил импорт

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.scss";
-import { Header, InputTodo } from "./Components/index";
+import { Header, InputTodo } from "./Components";
 
 function App() {
     return (
